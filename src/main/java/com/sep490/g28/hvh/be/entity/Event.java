61e70ee80001a23691d98bb586dcd92c18b40ea1
huyendieu8304 <huyendieu8304@gmail.com>
2026-03-25 14:51:45 +0700

    fix: set the event's end_date not null

--- src/main/java/com/sep490/g28/hvh/be/entity/Event.java
+++ src/main/java/com/sep490/g28/hvh/be/entity/Event.java
@@ -78,8 +78,7 @@ public class Event {
     @Column(name = "start_date", nullable = false)
     private LocalDate startDate;
 
-    //todo, suưửa ve not null after fix create event
-    @Column(name = "end_date", nullable = true)
+    @Column(name = "end_date", nullable = false)
     private LocalDate endDate;
 
     @Column(name = "recruitment_end_date", nullable = false)
