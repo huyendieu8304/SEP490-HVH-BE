6c90f31a389f75917b3167896272418d827b23d2
huyendieu8304 <huyendieu8304@gmail.com>
2026-03-27 21:39:07 +0700

    add: validation for reject event request

--- src/main/java/com/sep490/g28/hvh/be/dto/event/request/RejectEventRequest.java
+++ src/main/java/com/sep490/g28/hvh/be/dto/event/request/RejectEventRequest.java
@@ -1,6 +1,7 @@
 package com.sep490.g28.hvh.be.dto.event.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RejectEventRequest {
-    @NotBlank
+    @NotBlank(message = "INVALID_EVENT_REJECT_REASON")
+    @NotNull(message = "INVALID_EVENT_REJECT_REASON")
     String reason;
 }
