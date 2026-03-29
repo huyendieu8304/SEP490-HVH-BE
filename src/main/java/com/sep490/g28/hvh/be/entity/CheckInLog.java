95a6da3e9652b927f18f888731e6b5efbbd7fa46
kiennguyen <kienduccml@gmail.com>
2026-03-29 23:54:39 +0700

    fix: remove checkInAccuracyMeters from CheckInLog entity

--- src/main/java/com/sep490/g28/hvh/be/entity/CheckInLog.java
+++ src/main/java/com/sep490/g28/hvh/be/entity/CheckInLog.java
@@ -60,9 +60,6 @@ public class CheckInLog {
     )
     private Point checkInLocation;
 
-    @Column(name = "check_in_accuracy_meters", nullable = false)
-    private Double checkInAccuracyMeters;
-
     @CreationTimestamp
     @Column(
             name = "created_at",
