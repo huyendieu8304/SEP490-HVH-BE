97fc1947fbbbca27ba0137a020a52a9b4a182baf
huyendieu8304 <huyendieu8304@gmail.com>
2026-04-02 14:26:17 +0700

    add: checkAccountActive in AuthService

--- src/main/java/com/sep490/g28/hvh/be/service/AuthService.java
+++ src/main/java/com/sep490/g28/hvh/be/service/AuthService.java
@@ -2,6 +2,10 @@ package com.sep490.g28.hvh.be.service;
 
 import com.sep490.g28.hvh.be.dto.auth.request.ForgotPasswordRequest;
 
+import java.util.UUID;
+
 public interface AuthService {
     void forgotPassword(ForgotPasswordRequest request);
+
+    boolean checkAccountActive(UUID userId);
 }
