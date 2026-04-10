4d61a4e7ffbe634904e43ded0aefeba9e97d4f3a
kiennguyen <kienduccml@gmail.com>
2026-04-10 22:00:23 +0700

    feat: add getEventMomentsForVolunteer method

--- src/main/java/com/sep490/g28/hvh/be/service/EventMomentService.java
+++ src/main/java/com/sep490/g28/hvh/be/service/EventMomentService.java
@@ -1,8 +1,11 @@
 package com.sep490.g28.hvh.be.service;
 
 import com.sep490.g28.hvh.be.dto.eventmoment.request.ShareMomentRequest;
+import com.sep490.g28.hvh.be.dto.eventmoment.response.EventMomentFeedDetailsResponse;
 import com.sep490.g28.hvh.be.dto.eventmoment.response.EventMomentFeedResponse;
+import com.sep490.g28.hvh.be.dto.eventmoment.response.EventMomentResponseForVolunteer;
 import com.sep490.g28.hvh.be.dto.eventmoment.response.ShareMomentResponse;
+import org.springframework.data.domain.Page;
 
 import java.util.UUID;
 
@@ -12,4 +15,6 @@ public interface EventMomentService {
     EventMomentFeedResponse getEventMomentsFeed(int pageNumber, int pageSize, String eventName);
 
     void deleteEventMoment(UUID eventMomentId);
+
+    Page<EventMomentFeedDetailsResponse> getEventMomentsForVolunteer(int pageNumber, int pageSize, String eventName);
 }
