9b6d138b9c42183148240703613c3dd936e6157a
huyendieu8304 <huyendieu8304@gmail.com>
2026-04-23 14:55:44 +0700

    fix: merge dev

--- src/main/java/com/sep490/g28/hvh/be/service/AuthService.java
+++ src/main/java/com/sep490/g28/hvh/be/service/AuthService.java
@@ -1,9 +1,7 @@
 package com.sep490.g28.hvh.be.service;
 
 import com.sep490.g28.hvh.be.dto.auth.request.ChangePasswordRequest;
-import com.sep490.g28.hvh.be.dto.auth.request.ChangePhoneRequest;
 import com.sep490.g28.hvh.be.dto.auth.request.ForgotPasswordRequest;
-import jakarta.validation.Valid;
 
 import java.util.UUID;
 
