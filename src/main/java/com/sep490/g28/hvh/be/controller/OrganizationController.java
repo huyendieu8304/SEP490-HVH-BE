97c2218a40d9c81fbe5d81c7b9f6c02a9eff7401
kiennguyen <kienduccml@gmail.com>
2026-04-27 00:40:06 +0700

    add: add controller for updateOrganizationBySystemAdmin method

--- src/main/java/com/sep490/g28/hvh/be/controller/OrganizationController.java
+++ src/main/java/com/sep490/g28/hvh/be/controller/OrganizationController.java
@@ -2,6 +2,7 @@ package com.sep490.g28.hvh.be.controller;
 
 import com.sep490.g28.hvh.be.dto.organization.request.OrganizationRegistrationVerifyRequest;
 import com.sep490.g28.hvh.be.dto.organization.request.RegisterOrganizationRequest;
+import com.sep490.g28.hvh.be.dto.organization.request.UpdateOrganizationBySystemAdminRequest;
 import com.sep490.g28.hvh.be.dto.organization.response.*;
 import com.sep490.g28.hvh.be.service.OrganizationService;
 import com.sep490.g28.hvh.be.validation.OrganizationRegistrationStatus;
@@ -114,4 +115,18 @@ public class OrganizationController {
         return ResponseEntity.ok(organizationService.getOrganizationsBySystemAdmin(pageNumber, pageSize, name, orgTypes));
     }
 
+    @PreAuthorize("hasRole('SYS_ADMIN')")
+    @PutMapping("/sys-admin/organizations/{id}/update-profile")
+    public ResponseEntity<UpdateOrganizationBySystemAdminResponse> updateOrganizationBySystemAdmin(
+            @PathVariable(name = "id")
+            @UUID(message = "INVALID_UUID")
+            String inputId,
+
+            @RequestBody
+            @Valid
+            UpdateOrganizationBySystemAdminRequest request
+    ) {
+        java.util.UUID id = java.util.UUID.fromString(inputId);
+        return ResponseEntity.ok(organizationService.updateOrganizationBySystemAdmin(id, request));
+    }
 }
